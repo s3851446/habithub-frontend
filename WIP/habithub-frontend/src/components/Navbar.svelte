f20268acb51e1ab7f876bf01ed57693c24b800aa
Ayesha Babar <67376535+ayeshbabar@users.noreply.github.com>
2023-01-05 15:30:53 +0500

    Creates component Navbar.svelte ans uses it on homepage

---
+++ WIP/habithub-frontend/src/components/Navbar.svelte
@@ -0,0 +1,9 @@
+<nav>
+    <ul>
+        <li>
+            <a href="https://youtube.com">
+                <img src="./../../public/images/logo_app.png" alt=""/>
+            </a>
+        </li>
+    </ul>
+</nav>
