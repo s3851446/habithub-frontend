bd2095a69937250d3a31637316b0cd4c045a4d68
Ayesha Babar <67376535+ayeshbabar@users.noreply.github.com>
2023-01-06 16:29:04 +0500

    Adds unstyled navigation to project

--- WIP/habithub-frontend/src/components/Navbar.svelte
+++ WIP/habithub-frontend/src/components/Navbar.svelte
@@ -1,8 +1,33 @@
 <nav>
     <ul>
         <li>
-            <a href="https://youtube.com">
+            <a href="/">
                 <img src="./images/logo_app.png" alt=""/>
+                <span>Habit Hub</span>
+            </a>
+        </li>
+        <li>
+            <a href="/">
+                <i class="fa-solid fa-house"></i>
+                <span>Dashboard</span>
+            </a>
+        </li>
+        <li>
+            <a href="/habits">
+                <i class="fa-solid fa-list-check"></i>
+                <span>Habits</span>
+            </a>
+        </li>
+        <li>
+            <a href="/statistics">
+                <i class="fa-solid fa-chart-line"></i>
+                <span>Statistics</span>
+            </a>
+        </li>
+        <li>
+            <a href="/settings">
+                <i class="fa-solid fa-gears"></i>
+                <span>Settings</span>
             </a>
         </li>
     </ul>
