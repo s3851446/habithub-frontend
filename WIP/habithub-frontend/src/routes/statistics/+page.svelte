e7a5984ba3091aa2fd88779dac918b870f8bd219
DerrickTheys <20089759@student.curtin.edu.au>
2023-01-13 13:00:14 +1100

    uploading statistics page

--- WIP/habithub-frontend/src/routes/statistics/+page.svelte
+++ WIP/habithub-frontend/src/routes/statistics/+page.svelte
@@ -1,3 +1,218 @@
-<div class="body">
-    <h1>I am the Statistics page!</h1>
-</div>
+    <div class="body">
+      <section class="statistics">
+        <div class="content-wrap item-details divider">
+          <h2 id="statistics">Overall Progress</h2>
+        </div>
+        <div class="statistics-button">
+          <button type="button">Date range selector</button>
+        </div>
+        <div class="column">
+          <div class="new-container">
+            <h3>Workout</h3>
+            <hr>
+            <br>
+            <div class="variable-row">
+              <div class="variable-column">
+                <h4>Current Streak</h4>
+                <p>7 days</p>
+              </div>
+              <div class="variable-column">
+                <h4>Best Streak</h4>
+                <p>14 days</p>
+              </div>
+              <div class="variable-column">
+                <h4>Overall Progress</h4>
+                <p>7/30 Days</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="column">
+          <div class="new-container">
+            <h3>Workout</h3>
+            <hr>
+            <br>
+            <div class="variable-row">
+              <div class="variable-column">
+                <h4>Current Streak</h4>
+                <p>7 days</p>
+              </div>
+              <div class="variable-column">
+                <h4>Best Streak</h4>
+                <p>14 days</p>
+              </div>
+              <div class="variable-column">
+                <h4>Overall Progress</h4>
+                <p>7/30 Days</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="column">
+          <div class="new-container">
+            <h3>Workout</h3>
+            <hr>
+            <br>
+            <div class="variable-row">
+              <div class="variable-column">
+                <h4>Current Streak</h4>
+                <p>7 days</p>
+              </div>
+              <div class="variable-column">
+                <h4>Best Streak</h4>
+                <p>14 days</p>
+              </div>
+              <div class="variable-column">
+                <h4>Overall Progress</h4>
+                <p>7/30 Days</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+
+<style>
+
+/* Global Styles
+--------------------- */
+
+html {
+    box-sizing: border-box;
+}
+*, *:before, *:after {
+    box-sizing: inherit;
+}
+
+body {
+    margin: 0;
+    padding: 0;
+    font-size: 15px;
+    line-height: 1.5;
+    font-style: normal;
+}
+img {
+    width: 300px;
+}
+a:hover {
+    text-decoration: none;
+}
+h1, h2, h3, h4 {
+  line-height: 1;
+  margin: 10px;
+  text-align: center;
+}
+h2 {
+    padding-top: 20px;
+}
+h3 {
+  padding: 20px 0;
+}
+p {
+  text-align: left;
+}
+.content-wrap {
+    max-width: 95%;
+    width: 85%;
+    margin: 0 auto;
+    padding: 10px 0;
+    padding-bottom: 16px;
+}
+.item-details h3 ~ p {
+    margin: 0;
+}
+.divider > section {
+    padding: 25px 0;
+}
+.variable-column h4 {
+  text-align: left;
+}
+
+/* Statistics
+-----------------------------------*/
+
+.statistics {
+  padding: 24px 24px;
+}
+.column {
+  float: left;
+  width: 70%;
+  margin-bottom: 16px;
+  padding: 8px 8px;
+}
+.column h3 {
+  margin-bottom: 10px;
+  text-align: left;
+}
+.column h4 {
+  margin-left: 0;
+}
+
+@media screen and (max-width: 750px) {
+  .column {
+    width: 100%;
+    display: block;
+  }
+}
+.new-container {
+  padding: 0 16px;
+  border-radius: 5px;
+  background-color: #ffffff;
+  box-shadow: 2px 2px #d4d4d4;
+}
+.new-container::after, .row::after {
+  content: "";
+  clear: both;
+  display: table;
+}
+.statistics-button {
+  float: right;
+}
+.variable-column {
+  float: left;
+  width: 33%;
+}
+.variable-row:after {
+  content: "";
+  display: table;
+  clear: both;
+}
+
+/* Responsive Elements
+--------------------- */
+
+/* Display the columns on small screens */
+@media screen and (max-width: 900px) {
+  .column {
+    float: left;
+    width: 70%;
+    display: block;
+  }
+}
+@media screen and (max-width: 750px) {
+  .column {
+    float: left;
+    width: 100%;
+    display: block;
+  }
+}
+/* this is a media query that uses max-width. It triggers at 749 
+so it doesn't clash with the min-width. h1 font size reduces in size.
+It also changes line height of h2 and h1 and margin of h1.
+*/
+@media screen and (max-width:749px){
+    h1 {
+        font-size: 55px;
+        margin-bottom: 20px;
+        padding-left: 50px;
+    }
+    h1, h2 {
+        line-height: 1;
+    }
+    h2 {
+      font-size: 35px;
+    }
+}
+</style>
