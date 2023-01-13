8b58dc16e4b6824761f60db273d49c15f46549f3
Ayesha Babar <67376535+ayeshbabar@users.noreply.github.com>
2023-01-13 16:09:47 +0500

    Adds content to guide page

--- WIP/habithub-frontend/src/routes/guide/+page.svelte
+++ WIP/habithub-frontend/src/routes/guide/+page.svelte
@@ -1,3 +1,43 @@
+<script>
+    import Button from "../../components/Button.svelte";
+</script>
+
 <div class="body">
-    <h1>I am the guide page!</h1>
+    <h1>Welcome to Habit Hub!</h1>
+    <p>This is a quick guide to show you what you can do with Habit Hub!</p>
+    <div class="step">
+        <h2>Add/delete/edit habits</h2>
+        <!-- Placeholder from Adobe Stock Images:
+        https://stock.adobe.com/au/search?k=image+placeholder&search_type=usertyped&asset_id=268556012 -->
+    <img src="./images/img_placeholder.jpeg" alt="Placeholder">
+    </div>
+    <div class="step">
+        <h2>View daily/weekly/monthly stats</h2>
+        <!-- Placeholder from Adobe Stock Images:
+        https://stock.adobe.com/au/search?k=image+placeholder&search_type=usertyped&asset_id=268556012 -->
+    <img src="./images/img_placeholder.jpeg" alt="Placeholder">
+    </div>
+    <div class="btn">
+        <Button>Okay, got it!</Button>
+    </div>
 </div>
+
+<style>
+    h2 {
+        font-size: 17px;
+    }
+
+    img {
+        width: 600px;
+        height: 400px;
+        border-radius: 16px;
+    }
+
+    .step {
+        margin-bottom: 50px;
+    }
+
+    .btn {
+        margin-bottom: 20px;
+    }
+</style>
