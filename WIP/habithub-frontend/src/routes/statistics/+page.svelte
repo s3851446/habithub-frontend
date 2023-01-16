68f24c45a1bc699744e07a4efac5154771227bb6
Ayesha Babar <67376535+ayeshbabar@users.noreply.github.com>
2023-01-16 16:19:30 +0500

    Updates statistics page styles

--- WIP/habithub-frontend/src/routes/statistics/+page.svelte
+++ WIP/habithub-frontend/src/routes/statistics/+page.svelte
@@ -13,38 +13,24 @@
 </script>
 
 <div class="body">
-  <section class="statistics">
-    <div class="content-wrap item-details divider">
-      <h2 id="statistics">Overall Progress</h2>
-    </div>
-    <div class="statistics-button">
-      <Button class="range-selector">
-        <i class='bx bx-calendar'></i>
-        Data Range Selector
-      </Button>
-    </div>
-    <div class="progress-cards">
-      <ProgressCard />
-      <ProgressCard />
-      <ProgressCard />
-      <ProgressCard />
-    </div>
-  </section>
+  <div>
+    <h1>Overall Progress</h1>
+  </div>
+  <div class="statistics-button">
+    <Button class="range-selector">
+      <i class='bx bx-calendar'></i>
+      Data Range Selector
+    </Button>
+  </div>
+  <div class="progress-cards">
+    <ProgressCard />
+    <ProgressCard />
+    <ProgressCard />
+    <ProgressCard />
+  </div>
 </div>
 
 <style>
-  h2{
-    line-height: 1;
-    margin-bottom: 10px;
-  }
-
-  .content-wrap {
-      max-width: 95%;
-      width: 85%;
-      padding: 10px 0;
-      padding-bottom: 16px;
-  }
-
   .statistics-button {
     float: right;
   }
@@ -54,14 +40,7 @@
     margin-right: 7px;
   }
 
-  /* this is a media query that uses max-width. It triggers at 749 
-  so it doesn't clash with the min-width. h1 font size reduces in size.
-  It also changes line height of h2 and h1 and margin of h1.
-  */
-  @media screen and (max-width:749px){
-      h2 {
-          line-height: 1;
-          font-size: 35px;
-      }
+  .progress-cards {
+    margin-top: 30px;
   }
 </style>
