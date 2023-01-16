cd14476bf5bc93cb8f54c21ae25d0cc1101915b2
Ayesha Babar <67376535+ayeshbabar@users.noreply.github.com>
2023-01-16 20:44:00 +0500

    Completes styles of the settings page

--- WIP/habithub-frontend/src/routes/settings/+page.svelte
+++ WIP/habithub-frontend/src/routes/settings/+page.svelte
@@ -14,6 +14,7 @@
 <div class="body">
   <h1>Settings</h1>
     <section>
+      <!-- This is temporary & will be changed to a component soon. -AB -->
         <div class="container">
           <h2>Profile</h2>
           <hr>
@@ -44,23 +45,54 @@
         <div class="container">
           <h2>Notification settings</h2>
           <hr>
-        </div>
-      <!-- 
-          <div class="column">
-            <div class="new-container">
-              <h3></h3>
-              <hr>
-              <h4></h4>
-              <p>Reminders</p>
-              <p>Reminder Frequency</p>
-              <p>Reminder Time</p>
-              <hr>
-              <h4>Wake up at 6am</h4>
-              <p>Reminders</p>
-              <p>Reminder Frequency</p>
-              <p>Reminder Time</p>
+          <div class="container notification">
+            <div class="title">
+              <i class='bx bx-dumbbell'></i>
+              <h2>Workout</h2>
+            </div>
+            <hr>
+            <div class="wrapper">
+              <h3>Reminders</h3>
+              <p>On</p>
+            </div>
+            <div class="wrapper">
+              <h3>Reminder Frequency</h3>
+              <p>Daily</p>
+            </div>
+            <div class="wrapper">
+              <h3>Reminder Time</h3>
+              <p>09:00 AM</p>
             </div>
           </div>
+          <div class="container notification">
+            <div class="title">
+              <i class='bx bx-dumbbell'></i>
+              <h2>Workout</h2>
+            </div>
+            <hr>
+            <div class="wrapper">
+              <h3>Reminders</h3>
+              <p>On</p>
+            </div>
+            <div class="wrapper">
+              <h3>Reminder Frequency</h3>
+              <p>Daily</p>
+            </div>
+            <div class="wrapper">
+              <h3>Reminder Time</h3>
+              <p>09:00 AM</p>
+            </div>
+          </div>
+        </div>
+        <div class="container">
+          <h2>Colour Scheme</h2>
+          <hr>
+          <div class="wrapper">
+            <h3>Colour Scheme</h3>
+            <p>Light</p>
+          </div>
+        </div>
+      <!-- 
           <div class="column">
             <div class="new-container">
               <h3>Site Appearance</h3>
@@ -98,16 +130,21 @@
       margin: 20px 5px;
     }
 
-    p, h3 {
+    p, h3, h2 {
       margin: 0;
     }
 
+    hr {
+      margin-top: 16px;
+    }
+
     .container {
-      width: 280px;
+      width: 300px;
+      height: auto;
       background: $white;
       border-radius: $card-radius;
       box-shadow: $card-shadow;
-      padding: $card-padding;
+      padding: 16px;
       margin-top: 30px;
       h2 {
         font-size: 20px;
@@ -126,7 +163,7 @@
       align-items: flex-start;
       justify-content: flex-start;
       gap: 30px;
-      width: 70%;
+      width: 100%;
       transition: $trans-05;
       @media all and (max-width: 900px) {
         width: 100%;
@@ -134,5 +171,25 @@
         align-items: center;
         flex-direction: column;
       }
+    }
+
+    .notification {
+      width: auto;
+      margin-top: 15px;
+      .wrapper {
+        position: relative;
+      }
+    }
+
+    .title {
+        display: flex;
+        flex-flow: row nowrap;
+        justify-content: flex-start;
+        align-items: center;
+        column-gap: 16px;
+        i {
+            font-size: 25px;
+            color: $red;
+        }
     }
 </style>
