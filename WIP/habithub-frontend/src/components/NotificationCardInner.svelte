80da06aefa2078039972841586fd83139445c90c
Ayesha Babar <67376535+ayeshbabar@users.noreply.github.com>
2023-01-17 13:56:52 +0500

    Creates component for inner notification card

---
+++ WIP/habithub-frontend/src/components/NotificationCardInner.svelte
@@ -0,0 +1,77 @@
+ <div class="container notification">
+    <div class="title">
+        <i class='bx bx-dumbbell'></i>
+        <h2>Workout</h2>
+    </div>
+    <hr>
+    <div class="wrapper">
+        <h3>Reminders</h3>
+        <p>On</p>
+    </div>
+    <div class="wrapper">
+        <h3>Reminder Frequency</h3>
+        <p>Daily</p>
+    </div>
+    <div class="wrapper">
+        <h3>Reminder Time</h3>
+        <p>09:00 AM</p>
+    </div>
+</div>
+
+<style lang="scss">
+    .container {
+      width: 300px;
+      height: auto;
+      background: $white;
+      border-radius: $card-radius;
+      box-shadow: $card-shadow;
+      padding: 16px;
+      margin-top: 30px;
+      h2 {
+        font-size: 20px;
+      }
+      h3 {
+        font-size: 15px;
+      }
+      @media all and (max-width:900px) {
+        width: 100%;
+      }
+    }
+
+    .notification {
+      width: auto;
+      margin-top: 15px;
+      .wrapper {
+        position: relative;
+      }
+    }
+
+    .title {
+        display: flex;
+        flex-flow: row nowrap;
+        justify-content: flex-start;
+        align-items: center;
+        column-gap: 16px;
+        i {
+            font-size: 25px;
+            color: $red;
+        }
+    }
+
+    .wrapper {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        flex-flow: row wrap;
+        gap: 15px;
+        margin: 20px 5px;
+    }
+
+    p, h3, h2 {
+      margin: 0;
+    }
+
+    hr {
+      margin-top: 16px;
+    }
+</style>
--- WIP/habithub-frontend/src/routes/settings/+page.svelte
+++ WIP/habithub-frontend/src/routes/settings/+page.svelte
@@ -2,6 +2,7 @@
   import { validateToken } from './../../utils'
   import { onMount } from 'svelte'
   import Button from '../../components/Button.svelte';
+  import NotificationCard from '../../components/NotificationCardInner.svelte';
 
   onMount(async () => {
       const validToken = await validateToken()
@@ -48,44 +49,8 @@
         <div class="container">
           <h2>Notification settings</h2>
           <hr>
-          <div class="container notification">
-            <div class="title">
-              <i class='bx bx-dumbbell'></i>
-              <h2>Workout</h2>
-            </div>
-            <hr>
-            <div class="wrapper">
-              <h3>Reminders</h3>
-              <p>On</p>
-            </div>
-            <div class="wrapper">
-              <h3>Reminder Frequency</h3>
-              <p>Daily</p>
-            </div>
-            <div class="wrapper">
-              <h3>Reminder Time</h3>
-              <p>09:00 AM</p>
-            </div>
-          </div>
-          <div class="container notification">
-            <div class="title">
-              <i class='bx bx-dumbbell'></i>
-              <h2>Workout</h2>
-            </div>
-            <hr>
-            <div class="wrapper">
-              <h3>Reminders</h3>
-              <p>On</p>
-            </div>
-            <div class="wrapper">
-              <h3>Reminder Frequency</h3>
-              <p>Daily</p>
-            </div>
-            <div class="wrapper">
-              <h3>Reminder Time</h3>
-              <p>09:00 AM</p>
-            </div>
-          </div>
+          <NotificationCard />
+          <NotificationCard />
         </div>
         <div class="container">
           <h2>Colour Scheme</h2>
@@ -166,25 +131,5 @@
         align-items: center;
         flex-direction: column;
       }
-    }
-
-    .notification {
-      width: auto;
-      margin-top: 15px;
-      .wrapper {
-        position: relative;
-      }
-    }
-
-    .title {
-        display: flex;
-        flex-flow: row nowrap;
-        justify-content: flex-start;
-        align-items: center;
-        column-gap: 16px;
-        i {
-            font-size: 25px;
-            color: $red;
-        }
     }
 </style>
