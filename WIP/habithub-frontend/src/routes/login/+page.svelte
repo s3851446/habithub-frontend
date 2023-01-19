a4e56b75af513342d8f5f0472a097d317ca4a1a8
Ayesha Babar <67376535+ayeshbabar@users.noreply.github.com>
2023-01-19 19:37:45 +0500

    Adds logo to login and signup pages

--- WIP/habithub-frontend/src/routes/login/+page.svelte
+++ WIP/habithub-frontend/src/routes/login/+page.svelte
@@ -1,6 +1,7 @@
 <script>
     import { onMount } from 'svelte'
     import Button from "../../components/Button.svelte"
+  import TextInput from '../../components/TextInput.svelte';
     import { jwt, loggedIn, userFirstName } from './../../stores'
 
     onMount(() => {
@@ -65,8 +66,25 @@
 </script>
 
 <div class="body">
-    <h1>I am the Login page!</h1>
+    <img src="./images/logo_web.png" alt="">
+    <h1>Welcome back!</h1>
+    <p>It's so good to see you again!</p>
     <Button id="login_button">Test a login</Button>
 </div>
 
-<style lang="scss"></style>
+<style lang="scss">
+    img {
+        width: 300px;
+        height: auto;
+    }
+
+    .body {
+        padding: 0 40px;
+        margin-top: 0;
+        text-align: center;
+    }
+
+    h1 {
+        font-size: 20px;
+    }
+</style>
--- WIP/habithub-frontend/src/routes/signup/+page.svelte
+++ WIP/habithub-frontend/src/routes/signup/+page.svelte
@@ -1,5 +1,27 @@
+<script>
+    import TextInput from "../../components/TextInput.svelte";
+</script>
+
 <div class="body">
-    <h1>I am the Signup page!</h1>
+   <img src="./images/logo_web.png" alt="">
+    <h1>Sign up</h1>
+    <form action="">
+    </form>
 </div>
 
-<style lang="scss"></style>
+<style lang="scss">
+    img {
+        width: 300px;
+        height: auto;
+    }
+
+    .body {
+        padding: 0 40px;
+        margin-top: 0;
+        text-align: center;
+    }
+
+    h1 {
+        font-size: 20px;
+    }
+</style>
