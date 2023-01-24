34934c3486c87a8ed014b7a4ae6f69c12d6ded6b
ella watts <s3851446@student.rmit.edu.au>
2023-01-24 20:34:46 +1000

    include mohannad changes from branch/WIP

--- final/habithub-frontend/src/routes/login/+page.svelte
+++ final/habithub-frontend/src/routes/login/+page.svelte
@@ -3,6 +3,7 @@
     import Button from "../../components/Button.svelte"
     import TextInput from '../../components/TextInput.svelte';
     import { jwt, loggedIn, userFirstName } from './../../stores'
+    import { base } from '$app/paths'
 
     onMount(() => {
         const btn = document.getElementById('login_button')
@@ -59,13 +60,12 @@
             console.log("logged in successfully")
             window.location.href = '/'
 
-
             // put message somewhere if response bad
         }
     })
 </script>
 
-<div class="body">
+<!-- <div class="body">
     <img src="./images/logo_web.png" alt="">
     <h1>Welcome back!</h1>
     <p>It's so good to see you again!</p>
@@ -87,4 +87,72 @@
     h1 {
         font-size: 20px;
     }
-</style>
+</style> -->
+
+  <div class="container">
+    <img class="logo" src="{base}/images/logo_app.png" alt="Habit Hub logo" />
+    <form class="form">
+      <input class="form-control" type="email" placeholder="Email" id="email" />
+      <input class="form-control" type="password" placeholder="Password" id="password" />
+      <button class="btn" id="login_button">Login</button>
+    </form>
+    <div class="signup">
+      Don't have an account? <a href="/signup">Sign Up</a>
+    </div>
+  </div>
+
+  <style>
+    .container {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      height: 100vh;
+      background: radial-gradient(circle at 30% 107%, #ef6461, #e4b363);
+    }
+  
+    .logo {
+      width: 200px;
+      margin-bottom: 20px;
+    }
+  
+    .form {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      width: 300px;
+      margin-bottom: 20px;
+    }
+  
+    .form-control {
+      margin-bottom: 10px;
+      width: 100%;
+      padding: 12px 20px;
+      box-sizing: border-box;
+      border: 2px solid #e4b363;
+      border-radius: 4px;
+    }
+  
+    .btn {
+      background-color: #e4b363;
+      color: #fff;
+      padding: 12px 20px;
+      border: none;
+      border-radius: 4px;
+      cursor: pointer;
+      font-size: 16px;
+      width: 100%;
+      transition: background-color 0.2s ease;
+    }
+  
+    .btn:hover {
+      background-color: #ef6461;
+    }
+  
+    .signup {
+      margin-top: 20px;
+      font-size: 14px;
+      text-align: center;
+    }
+  
+  </style>
