773b32eaeb20e8e3a6a81d0b262aa905c450135b
Ayesha Babar <67376535+ayeshbabar@users.noreply.github.com>
2023-01-25 18:09:09 +0500

    Adds PopUp.svelte for form editing purposes

---
+++ final/habithub-frontend/src/components/ImageUpload.svelte
@@ -0,0 +1,38 @@
+<script>
+    let file = null;
+
+    function handleFileUpload(){
+        //code for handling file upload
+    }
+
+</script>
+
+<div class="input_container">
+    <label for="file" class="custom-file-upload">Upload profile picture</label>
+    <input type="file" id="file" bind:value={file}>
+</div>
+
+
+
+<style lang=scss>
+    .input_container {
+        display: flex;
+        flex-flow: column nowrap;
+        justify-content: center;
+        align-items: center;
+        gap: 0;
+        transition: $trans-05;
+    }
+
+    input[type="file"] {
+    visibility: hidden;
+    }
+    
+    .custom-file-upload {
+        cursor: pointer;
+        display: inline-block;
+        padding: 8px 12px;
+        border: 2px solid $orange;
+        border-radius: 4px;
+    }
+</style>
