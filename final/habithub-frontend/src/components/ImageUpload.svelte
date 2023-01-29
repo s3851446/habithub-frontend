1fa68123e60388da984cb1a7e64eeca28f492e66
ella watts <s3851446@student.rmit.edu.au>
2023-01-29 21:15:04 +1000

    add notes

--- final/habithub-frontend/src/components/ImageUpload.svelte
+++ final/habithub-frontend/src/components/ImageUpload.svelte
@@ -1,9 +1,13 @@
 <script>
     export let file = null
 
-    function handleFileUpload(){
-        //code for handling file upload
-    }
+    // function handleFileUpload(){
+    //     //code for handling file upload
+    // }
+
+    //NOTE - should be checking for filetype before allowing file upload 
+    // only really want to allow png/jpeg
+    // might also be worth restricting file size
 
 </script>
 
