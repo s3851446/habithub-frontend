a532b9c38d7cfc432b69ddf94e46899f9248e09f
ella watts <s3851446@student.rmit.edu.au>
2023-01-31 07:58:11 +1000

    fix url from test

--- final/habithub-frontend/src/routes/settings/+page.svelte
+++ final/habithub-frontend/src/routes/settings/+page.svelte
@@ -66,8 +66,8 @@
     if (files) {
       const formData = new FormData()
       formData.append('profilepic', files[0])
-      // const fileResponse = await fetch(`https://habithub-api.herokuapp.com/pic/${userID}`, {
-      const fileResponse = await fetch(`http://localhost:3000/pic/${userID}`, {
+      const fileResponse = await fetch(`https://habithub-api.herokuapp.com/pic/${userID}`, {
+      // const fileResponse = await fetch(`http://localhost:3000/pic/${userID}`, {
         method: 'POST',
         headers: {
           'Authorization': 'BEARER ' + jwt
