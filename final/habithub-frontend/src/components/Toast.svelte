f73d7bb3d63d574b567515f7f164266cd0966cad
Ayesha Babar <67376535+ayeshbabar@users.noreply.github.com>
2023-01-30 15:21:23 +0500

    Creates Toast.svelte component

---
+++ final/habithub-frontend/src/components/Toast.svelte
@@ -0,0 +1,73 @@
+<script>
+    import { fade } from 'svelte/transition';
+
+    let showToast = false;
+    let error = false;
+
+    setTimeout(() => {
+        showToast = false;
+    }, 2000);
+</script>
+
+{#if showToast}
+    <div class="toast {error ? 'error' : ''}" transition:fade|local={{duration: 300}}>
+        <div class="toast-header">
+            <p><b>Account Created!</b> Please sign in.</p>
+            <i 
+                class="bx bx-x toast-close" 
+                on:click={() => showToast = false} 
+                on:keydown={(e) => {
+                    if (e.key === 'Enter' || e.key === ' ') {
+                        showToast = false;;
+                    }
+                }}>
+            </i>
+        </div>
+    </div>
+{/if}
+
+<style lang=scss>
+    .toast {
+        position: fixed;
+        top: 20px;
+        left: 0;
+        right: 0;
+        margin: auto;
+        width: 80%;
+        max-width: 300px;
+        background: $white;
+        box-shadow: $card-shadow;
+        padding: 10px 20px;
+        border-radius: $card-radius;
+        z-index: 200;
+        transition: top 0.3s ease-in-out;
+        &.error {
+            background: $red;
+            color: $white;
+        }
+        @media (max-width: 576px) {
+            width: 90%;
+        }
+        @media (min-width: 577px) and (max-width: 768px) {
+            width: 70%;
+        }
+        @media (min-width: 769px) and (max-width: 992px) {
+                width: 60%;
+            }
+    }
+
+    .toast-header {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+    }
+
+    .toast-close {
+        font-size: 18px;
+        cursor: pointer;
+    }
+
+    p {
+        margin: 0;
+    }
+</style>
