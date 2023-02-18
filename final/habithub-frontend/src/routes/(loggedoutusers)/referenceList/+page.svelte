df7352156dfc9f75908734f076fb59bfe095072b
Ayesha Babar <67376535+ayeshbabar@users.noreply.github.com>
2023-02-18 12:36:35 +0500

    Adds back accidentally deleted references

--- final/habithub-frontend/src/routes/(loggedoutusers)/referenceList/+page.svelte
+++ final/habithub-frontend/src/routes/(loggedoutusers)/referenceList/+page.svelte
@@ -1,35 +1,93 @@
 <div class="reference">
   <div class="content">
     <h1>Reference List</h1>
-    <h2>Svg and Images</h2>
-    <p>
-      The visual elements featured on this website, including all SVGs and
-      images, were sourced from Adobe Stock Images. Our authors have made
-      modifications to the images and SVGs to align them with the overall theme
-      of the website. <br />
-      <b
-        >Adobe Stock. n.d. “Find the perfect stock asset for your next creative
-        project.” Adobe.
-        <a href="https://stock.adobe.com/au/">https://stock.adobe.com/au/</a
-        >.</b
-      >
-    </p>
-    <h2>Website Components</h2>
-    <p>
-      The incorporation of certain components, such as the footer, throughout
-      the website was achieved through the utilisation of YouTube tutorials.
-      These tutorials have been properly referenced within the code comments for
-      ease of review and transparency. Kindly refer to the code comments for
-      further information.
-    </p>
-    <h2>Animations</h2>
-    <p>
-      The animated depictions of the young man utilised throughout the website
-      were created utilising the tool known as SVGator. <br /><b
-        >SVGator. n.d. “The Easiest Way to Animate SVG.” SVGator.
-        <a href="https://www.svgator.com/">https://www.svgator.com/</a>.</b
-      >
-    </p>
+    <div class="section">
+      <h2>Svg and Images</h2>
+      <p>
+        The visual elements featured on this website, including all SVGs and
+        images, were sourced from Adobe Stock Images. Our authors have made
+        modifications to the images and SVGs to align them with the overall
+        theme of the website.
+      </p>
+      <ul>
+        <li>
+          Adobe Stock. n.d. “Find the perfect stock asset for your next creative
+          project.” Adobe.
+          <a href="https://stock.adobe.com/au/">https://stock.adobe.com/au/</a>.
+        </li>
+      </ul>
+    </div>
+    <div class="section">
+      <h2>Website Components</h2>
+      <p>
+        The incorporation of certain components, such as the footer, throughout
+        the website was achieved through the utilisation of YouTube tutorials.
+        These tutorials have been properly referenced within the code comments
+        for ease of review and transparency. Kindly refer to the code comments
+        for further information.
+      </p>
+    </div>
+    <div class="section">
+      <h2>Animations</h2>
+      <p>
+        The animated depictions of the young man utilised throughout the website
+        were created utilising the tool known as SVGator.
+      </p>
+      <ul>
+        <li>
+          SVGator. n.d. “The Easiest Way to Animate SVG.” SVGator.
+          <a href="https://www.svgator.com/">https://www.svgator.com/</a>.
+        </li>
+      </ul>
+    </div>
+    <div class="section">
+      <h2>JavaScript</h2>
+      <p>
+        Several websites have been used to solve JavaScript problems and bugs.
+      </p>
+      <ul>
+        <li>
+          Ahmed, I. 2021. “Using Fetch API to create a Login form.” Stack
+          Overflow.
+          <a
+            href="https://stackoverflow.com/questions/68389117/using-fetch-api-to-create-a-login-form"
+          >
+            https://stackoverflow.com/questions/68389117/using-fetch-api-to-create-a-login-form
+          </a>
+        </li>
+        <li>
+          Kantor, I. 2021. “FormData.” JavaScript.Info.
+          <a href="https://javascript.info/formdata">
+            https://javascript.info/formdata
+          </a>
+        </li>
+        <li>
+          Mozilla. n.d. “History.replaceState().” Mozilla.
+          <a
+            href="https://developer.mozilla.org/en-US/docs/Web/API/History/replaceState"
+          >
+            https://developer.mozilla.org/en-US/docs/Web/API/History/replaceState
+          </a>
+        </li>
+        <li>
+          Mozilla. n.d. “Using the Fetch API.” Mozilla.
+          <a
+            href="https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch"
+          >
+            https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+          </a>
+        </li>
+        <li>
+          u/SillyFreak. 2022. “Best way to change URL (in SvelteKit) in a
+          multi-step process without resetting the page.” Reddit.
+          <a
+            href="https://www.reddit.com/r/sveltejs/comments/t2oeqg/best_way_to_change_url_in_sveltekit_in_a/"
+          >
+            https://www.reddit.com/r/sveltejs/comments/t2oeqg/best_way_to_change_url_in_sveltekit_in_a/
+          </a>
+        </li>
+      </ul>
+    </div>
   </div>
   <div class="man">
     <object title="Man typing" type="image/svg+xml" data="./images/typing.svg">
@@ -42,6 +100,7 @@
   a {
     color: $red;
     text-decoration: none;
+
     &:hover {
       text-decoration: underline;
     }
@@ -49,12 +108,13 @@
 
   h2 {
     font-size: 17px;
+    margin-bottom: 5px;
   }
 
   .reference {
     display: flex;
     flex-direction: row;
-    align-items: flex-end;
+    align-items: center;
     justify-content: flex-start;
     gap: 20px;
     .content {
@@ -73,6 +133,19 @@
       }
       .content {
         width: 100%;
+      }
+    }
+  }
+
+  .section {
+    margin-bottom: 20px;
+    ul {
+      margin: -10px 0 0 30px;
+      li {
+        padding-top: 10px;
+        &:first-of-type {
+          padding-top: 0;
+        }
       }
     }
   }
